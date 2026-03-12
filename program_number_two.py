15bdec5513b08a259dff0129d53b656e0f488b1f
John Wilson Cristobal <johnwilsoncristobal@gmail.com>
2026-03-12 11:16:44 +0800

    Print equal if both numbers are equal and not equal if not

---
+++ program_number_two.py
@@ -0,0 +1,9 @@
+#Input two numbers
+number1=float(input("Enter a number: "))
+number2=float(input("Enter another number: "))
+
+#Check if both numbers are equal or not
+if number1 == number2:
+    print("Both numbers are equal")
+else:
+    print("Both numbers are not equal")
