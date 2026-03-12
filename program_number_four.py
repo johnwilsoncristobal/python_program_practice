c8a99d8ca503e7afbd32ced8ab26a3d26d4334d0
John Wilson Cristobal <johnwilsoncristobal@gmail.com>
2026-03-12 11:23:11 +0800

    Print the product of the two numbers

---
+++ program_number_four.py
@@ -0,0 +1,9 @@
+# Input two numbers
+number1 = float(input("Enter a number: "))
+number2 = float(input("Enter another number: "))
+
+# Multiply the two numbers
+product = number1 * number2
+
+# Print the product of the two numbers
+print(f"The product is: {product}")
