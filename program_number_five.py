8949eda34ce78a4eae8ffcd8fb6dac1f21b931ca
John Wilson Cristobal <johnwilsoncristobal@gmail.com>
2026-03-12 11:27:46 +0800

    Print the quotient of the two numbers

---
+++ program_number_five.py
@@ -0,0 +1,9 @@
+# Input two numbers
+number1 = float(input("Enter a number: "))
+number2 = float(input("Enter another number: "))
+
+# Divide the two numbers
+quotient = number1 / number2
+
+# Print the quotient of the two numbers
+print(f"The quotient is: {quotient}")
