ddbe6d7ba61e041982011ceaa85a3c91bdc0a0ca
John Wilson Cristobal <johnwilsoncristobal@gmail.com>
2026-03-12 11:19:58 +0800

    Print the sum of the two numbers

---
+++ program_number_three.py
@@ -0,0 +1,9 @@
+# Input two numbers
+number1 = float(input("Enter a number: "))
+number2 = float(input("Enter another number: "))
+
+# Sum up the two numbers
+sum = number1 + number2
+
+# Print the sum of the two numbers
+print(f"The sum is: {sum}")
