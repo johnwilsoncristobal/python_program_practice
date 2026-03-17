4713bb023679e472424ba403ef7be0c749e979fb
John Wilson Cristobal <johnwilsoncristobal@gmail.com>
2026-03-17 14:40:21 +0800

    Result when the first number is raised to the second number

---
+++ program_number_six.py
@@ -0,0 +1,9 @@
+# Input two numbers
+number1 = float(input("Enter a number: "))
+number2 = float(input("Enter another number: "))
+
+# Raise the first number to the second number
+result= number1 ** number2
+
+# Print the result
+print(f"The result is: {result}")
