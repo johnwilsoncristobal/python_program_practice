1f2394e9d44ca7770cc428a2e875c0ef47c7bf8a
John Wilson Cristobal <johnwilsoncristobal@gmail.com>
2026-03-20 13:56:35 +0800

    Remove program_number_fibe.py

--- program_number_fibe.py
+++
@@ -1,9 +0,0 @@
-# Input two numbers
-number1 = float(input("Enter a number: "))
-number2 = float(input("Enter another number: "))
-
-# Divide the two numbers
-quotient = number1 / number2
-
-# Print the quotient of the two numbers
-print(f"The quotient is: {quotient}")
